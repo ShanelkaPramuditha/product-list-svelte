41f0725084ad85508f0348acfcc462e5c36e943e
Shanelka Pramuditha <99pramuditha@gmail.com>
2024-08-31 00:38:11 +0530

    chore: Update "Add to Cart" button color on product page

--- src/routes/product/[id]/+page.svelte
+++ src/routes/product/[id]/+page.svelte
@@ -56,7 +56,7 @@
 						<div class="flex -mx-2 mb-4">
 							<div class="w-1/2 px-2">
 								<button
-									class="w-full bg-gray-900 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800"
+									class="w-full bg-blue-700 text-white py-2 px-4 rounded-full font-bold hover:bg-blue-800"
 									on:click={() => handleAddToCart(product)}>Add to Cart</button
 								>
 							</div>
